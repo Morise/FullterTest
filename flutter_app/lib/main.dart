8264a1d785fb90eaea45a788c4345d0267c4522a
jiangdongbo <771524358@qq.com>
2019-01-07 11:02:20 +0800

    test2s

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-//test2
+//test2s
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
